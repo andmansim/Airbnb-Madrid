1c1339ac7d4ce721c5fdb5366ca3c2ab1cb09f36
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-09 21:08:21 +0200

    cambiar precio y limpiar

--- pand.py
+++ pand.py
@@ -7,7 +7,7 @@ Preprocesar el fichero de alojamientos para crear un data frame con las variable
 room_type, neighbourhood_group_cleansed, price, cleaning_fee, accommodates, minimum_nights, 
 minimum_cost, review_scores_rating, latitude, longitude, is_location_exact. 
 Eliminar del data frame cualquier fila incompleta. 
-Añadir al data frame nuevas variables con el coste mínimo por noche y por persona ç
+Añadir al data frame nuevas variables con el coste mínimo por noche y por persona 
 (que incluya los gastos de limpieza).
 '''
 df = pd.read_csv('madrid.csv', delimiter ='\t', encoding = 'UTF-8')
@@ -17,4 +17,13 @@ df.rename(columns= {'id': 'id_aloj', 'host_id': 'id_anfi', 'listing_url': 'url',
 'minimum_nights': 'noches_min', 'review_scores_rating': 'puntuacion', 'latitude': 'latitud',
 'longitude': 'longitud', 'is_location_exact': 'localizacion_exact'}, inplace=True)
 df1 = df[['id_aloj','id_anfi', 'url', 'tipo_hab', 'distrito', 'precio', 'limpiar', 'plazas', 'noches_min', 'puntuacion']]
-print(df1)
+#eliminar
+df1= df1.dropna()
+#reemplazar
+
+df1['precio'] = df1.precio.str.replace(',', '.')
+df1['precio'] = df1.precio.str.replace('$', '')
+print(df['precio'])
+df1['limpiar'] = df1.limpiar.str[1:].astype('float')
+print(df1['limpiar'])
+
