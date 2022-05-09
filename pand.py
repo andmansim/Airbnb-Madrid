0a1da3ab29bc0e1be0e90ab278f38c50dfd630ad
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-09 20:57:13 +0200

    corregido error

--- pand.py
+++ pand.py
@@ -10,16 +10,11 @@ Eliminar del data frame cualquier fila incompleta.
 Añadir al data frame nuevas variables con el coste mínimo por noche y por persona ç
 (que incluya los gastos de limpieza).
 '''
-
-
-
 df = pd.read_csv('madrid.csv', delimiter ='\t', encoding = 'UTF-8')
-print (df)
 
-'''df.rename(columns= {'id': 'id_aloj', 'host_id': 'id_anfi', 'listing_url': 'lista_url', 
+df.rename(columns= {'id': 'id_aloj', 'host_id': 'id_anfi', 'listing_url': 'url', 
 'room_type' : 'tipo_hab', 'neighbourhood_group_cleansed': 'distrito', 'price':'precio', 'cleaning_fee':'limpiar', 'accommodates': 'plazas',
-'minimum_nights': 'noches_min','minimum_cost': 'cost_min', 'review_scores_rating': 'comprobar_nota', 'latitude': 'latitud',
+'minimum_nights': 'noches_min', 'review_scores_rating': 'puntuacion', 'latitude': 'latitud',
 'longitude': 'longitud', 'is_location_exact': 'localizacion_exact'}, inplace=True)
-df1 = df[['id_aloj','id_anfi', 'lista_url', 'tipo_hab', 'distrito', 'precio', 'limpiar', 'plazas', 'noches_min',
-          'cost_min', 'comprobar_nota', 'latitud', 'longitud', 'localizacion_exact']]
-print(df1)'''
+df1 = df[['id_aloj','id_anfi', 'url', 'tipo_hab', 'distrito', 'precio', 'limpiar', 'plazas', 'noches_min', 'puntuacion']]
+print(df1)
