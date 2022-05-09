67f0187ed74e5998ebdd9a02daddb613493ce7f7
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-09 18:51:25 +0200

    intento 3

--- funciones.py
+++ funciones.py
@@ -1,17 +1,15 @@
-
-
+import csv
+# Sin pandas
 '''
-Sin usar Pandas:
-
 1. Extraer del fichero de alojamientos una lista con todos los alojamientos, 
 donde cada alojamiento sea un diccionario que contenga el identificador del alojamiento, 
 el identificador del anfitrión, el distrito, el precio y las plazas.
 '''
-def informacion(): 
-    file = open('madrid-airbnb-listings-small.csv') 
-    #leer = csv.DictReader(file, delimiter = '\t') #delimiter, para indicar los separadores
+'''def informacion(): 
+    file = open('madrid-airbnb-listings-small.csv', encoding='UTF-8') 
+    leer = csv.DictReader(file, delimiter = '\t') #delimiter, para indicar los separadores
     leer = file.readlines()
-    file.close()
+    
     columnas = leer[0].split('\t')
     datos = ['id', 'host_id', 'neighbourhood_group_cleansed', 'accommodates', 'price']
     traduccion = {'id': 'id_aloj', 'host_id': 'id_anfi', 'neighbourhood_group_cleansed': 'distrito', 
@@ -27,11 +25,42 @@ def informacion():
         lista.append(lista)
     
     return lista
+'''
+def informacion():
+    lista = []
+    with open('madrid-airbnb-listings-small.csv', encoding='UTF-8') as file:
+        leer = csv.DictReader(file, delimiter = '\t')
+        
+    for i in leer:
+        lista.append(i)
 
+lista_aloj = informacion()
+def modificacion(lista_aloj):
+    for a in lista_aloj:
+        a['id_anfi'] = a['id']
+        a['id_aloj'] = a['host_id']
+        a['distrito'] = a['neighbourhood_group_cleansed']
+        a['precio'] = a['price']
+        a['plazas'] = a['accommodates']   
+        nuevos = ['id_anfi', 'id_aloj', 'distrito', 'precio', 'plazas' ]
+        if a.keys not in nuevos:
+            del(a.keys)
 
-
-lista = informacion()
-for x in range(len(lista)):
-    print(lista[x])
-    print('\n')
-
+    print(lista_aloj)
+    return lista_aloj
+    
+lista_aloj1 = modificacion(lista_aloj)   
+print(lista_aloj)
+'''for x in range(len(lista_aloj)):
+    print(lista_aloj[x])
+    print('\n')'''
+    
+'''
+2. Crear una función que reciba la lista de alojamientos y devuelva el número de alojamientos en cada distrito.
+'''
+def numdistrito(lista_aloj):
+    l_d = []
+    for a in range(len(lista_aloj)):
+        distrito = a.get('distrito')
+        l_d.append(distrito)
+        
