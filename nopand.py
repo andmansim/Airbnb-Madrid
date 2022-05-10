37cb4910552ce0cea67d0510f498050e0b2b9128
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-10 17:47:34 +0200

    funcion ocupantes

--- nopand.py
+++ nopand.py
@@ -35,11 +35,7 @@ def modificacion(lista_aloj):
             del(a[c])
  
     
-lista_aloj = informacion()    
-modificacion(lista_aloj)   
-for x in range(len(lista_aloj)):
-    print(lista_aloj[x])
-    print('\n')
+
     
 '''
 2. Crear una función que reciba la lista de alojamientos y devuelva el número de alojamientos en cada distrito.
@@ -51,15 +47,39 @@ def numdistrito(lista_aloj):
         l_d.append(distrito)
     n = Counter(l_d)
     return n
-num = numdistrito(lista_aloj)
-print('El número de cada distrito es: ' + str(num))
+
 
 '''
 3. Crear una función que reciba la lista de alojamientos y un número de ocupantes y
 devuelva la lista de alojamientos con un número de plazas mayor o igual que el número de ocupantes.
 '''
+def ocupantes(lista_aloj, n):
+    l_aloj = []
+    for a in lista_aloj:
+        plaza = a['plazas']
+        if int(plaza) >= n:
+            l_aloj.append(a)
+    return l_aloj
 
+lista_aloj = informacion()    
+modificacion(lista_aloj)
+print('Todos los alojamientos:')   
+for x in range(len(lista_aloj)):
+    print(lista_aloj[x])
+    print('\n')
 
+num = numdistrito(lista_aloj)
+print('El número de cada distrito es: ' + str(num))
+
+l_aloj = ocupantes(lista_aloj, 5)
+print('\n')
+print('Los alojamientos disponibles son: ')
+for y in range(len(l_aloj)):
+    print(l_aloj[y])
+    w = w + 1
+    print('\n')
+
+    
 '''
 4. Crear una función que reciba la lista de alojamientos un distrito, y 
 devuelva los 10 alojamientos más baratos del distrito.
